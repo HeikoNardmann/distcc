a8ed8ff4c68b26863358ea44e289b0e960a614d2
Heiko Nardmann <heiko.nardmann@itechnical.de>
2016-05-31 11:04:43 +0200

    intermediate state

---
+++ cmake/CheckForVaCopy.cmake
@@ -0,0 +1,34 @@
+MACRO(CHECK_FOR_VA_COPY RESULT)
+  MESSAGE(STATUS "Check for va_copy")
+  TRY_COMPILE(${RESULT} ${CMAKE_BINARY_DIR}
+    ${CMAKE_SOURCE_DIR}/cmake/CheckForVaCopy.c)
+  IF(${RESULT})
+    MESSAGE(STATUS "Check for va_copy - found")
+  ELSE()
+    MESSAGE(STATUS "Check for va_copy - not found")
+  ENDIF()
+
+#    if(HAVE_VA_COPY)
+#        SET(VA_COPY va_copy CACHE STRING "va_copy function")
+#    else()
+#        write_file("${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/cmake_try_compile.c"
+#        "#include <stdarg.h>
+#        void f (int i, ...) {
+#            va_list args1, args2;
+#            va_start (args1, i);
+#            __va_copy (args2, args1);
+#            if (va_arg (args2, int) != 42 || va_arg (args1, int) != 42)
+#                exit (1);
+#            va_end (args1); va_end (args2);
+#        }
+#        int main() {
+#            f (0, 42);
+#            return 0;
+#        }
+#        ")
+#        try_compile(HAVE_VA_COPY ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/cmake_try_compile.c)
+#        if(HAVE___VA_COPY)
+#            SET(_VA_COPY __va_copy CACHE STRING "va_copy function")
+#        endif()
+#    endif()
+ENDMACRO()
